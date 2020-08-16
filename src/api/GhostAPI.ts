26714bed8154c1e30c3175e0f9841dfb5453ab35
Oscar Newman <newman.oscar@gmail.com>
2020-08-16 13:15:56 -0400

    Disable more ghost

--- src/api/GhostAPI.ts
+++ src/api/GhostAPI.ts
@@ -1,8 +1,10 @@
 import GhostContentAPI from '@tryghost/content-api'
 
 // Create API instance with site credentials
-export const Ghost = new GhostContentAPI({
-  url: process.env.GHOST_URL,
-  key: process.env.GHOST_KEY,
-  version: 'v3',
-})
+// export const Ghost = new GhostContentAPI({
+//   url: process.env.GHOST_URL,
+//   key: process.env.GHOST_KEY,
+//   version: 'v3',
+// })
+
+const Ghost = {}
